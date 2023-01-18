9a362f4b9b66a9437d8668730389953dda531132
code-Raghav <araghav2003@gmail.com>
2023-01-18 23:01:34 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
 
-//   // Add basePath
-//   basePath: "/github-pages",
-// };
+  // Add basePath
+  basePath: "/github-pages",
+};
 
-// module.exports = nextConfig;
+module.exports = nextConfig;
